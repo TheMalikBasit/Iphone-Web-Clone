f92b14f0359ebd64539836e395be211d330591c9
MALIK ABDUL BASIT <malikabdulbasit03@gmail.com>
2025-04-02 02:36:21 +0500

    added 3 screens

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,13 @@
-import Home from '@/Home'
+import Hero from "@/Components/Hero"
+import Highlights from "@/Components/Highlights"
+import Navbar from "@/Components/Navbar"
 
 export default function Main() {
   return (
-    <div className='bg-black text-white p-4 flex flex-col justify-center items-center'>
-      <header className='flex flex-col items-center w-full'>
-        <p>Main</p>
-        <div className='flex flex-row items-center justify-between w-full'>
-          <Home />
-          <p className='m-10'>I should be on right</p>
-        </div>
-      </header>
-      <footer className='flex flex-col items-center justify-center w-full p-20'>
-        <p className='text-md sm:text-left font-[family-name:var(--font-geist-mono)] text-primary p-5'>Footer</p>
-        <div className='flex flex-row items-center justify-between w-full'>
-          <p className='text-md sm:text-left font-[family-name:var(--font-geist-mono)] text-gray'>I should be on left</p>
-          <p className='text-md sm:text-left font-[family-name:var(--font-geist-mono)] text-secondary'>I should be on right</p>
-        </div>
-      </footer>
-    </div>
-    
+    <main>
+      <Navbar />
+      <Hero />
+      <Highlights />  
+    </main>
   )
 }
