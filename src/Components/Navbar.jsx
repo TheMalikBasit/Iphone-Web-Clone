8ee01069fc004347a1593d8c702c92472526b84c
MALIK ABDUL BASIT <malikabdulbasit03@gmail.com>
2025-04-08 01:43:56 +0500

    Literally Nothing

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,10 @@
 
 export default function Navbar() {
     return (
-        <div className="flex flex-row justify-between items-center w-full">
-            <button className="p-10 border-2 border-accent bg-blue-500 text-secondary">Navnar</button>
+        <div className="flex items-center">
+            <div>
+                <button>I am navbar</button>
+            </div>
         </div>
-    )
+    );
 }
